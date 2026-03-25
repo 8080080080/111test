4b83ff0d3b1275894657bc2b8681abfe60c9e096
sandro <s_ekhvaia@cu.edu.ge>
2026-03-25 21:39:45 +0400

    jsonn fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,9 @@ import siteData from "./src/data/site.json" with { type: "json" };
 
 export default defineConfig({
   site: siteData.defaultSeo.siteUrl,
-
   image: {
     responsiveStyles: true
   },
-
   output: "static",
-  trailingSlash: "always",
-  adapter: cloudflare()
+  trailingSlash: "always"
 });
